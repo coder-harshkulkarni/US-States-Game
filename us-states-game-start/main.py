971a8dd49f50bf34654cff567d2eea1a81834262
“Harsh <harshkulkarni78@gmail.com>
2023-02-20 14:49:04 +0530

    Add list comprehension instead of multi-line code

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -9,14 +9,15 @@ screen.addshape(image)
 turtle.shape(image)
 guessed_states = []
 all_states = data['state'].to_list()
-missing_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # This one line of code word same as commented code
+        missing_states = [state for state in all_states if (state not in guessed_states)]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         df = pandas.DataFrame(missing_states)
         df.to_csv("states_to_learn.csv")
         break
